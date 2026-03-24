8fefbda8e4e8e48fe98095e3182e6bf58c652ed6
Pranamya-rao <raopranamya24@gmail.com>
2026-03-24 09:32:46 +0530

    fourth assignment

---
+++ module 13/Printer.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+import java.util.Queue;
+
+public class Printer {
+    public static void main(String[] args) {
+        Queue<String> q = new LinkedList<>();
+
+        q.add("Sapthagiri ");
+        q.add("NPS ");
+        q.add("University ");
+
+        while (!q.isEmpty()) {
+            System.out.println("Printing: " + q.poll());
+        }
+    }
+}
